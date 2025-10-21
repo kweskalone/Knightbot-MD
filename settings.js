be6581e67e02d7d75ed71f6875cdf85b311787de
Skalone <abubakarimohammed2004@gmail.com>
2025-10-21 13:31:22 +0000

    commit bot files

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'Knight Bot',
+  author: 'Skalone',
+  botName: "Knight Bot",
+  botOwner: 'Skalone', // Your name
+  ownerNumber: '233208459757', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "private",
+  maxStoreMessages: 100, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.2.2",
+  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
